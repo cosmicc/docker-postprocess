e182d7c16bf24b42cc5106383e652c626f4b13e5
unknown <ianperry99@gmail.com>
2019-11-24 12:25:35 -0500

    update

--- completemovies.sh
+++ completemovies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 /opt/mp4_automator/manual.py -i /downloads/complete/movies -c /config/autoProcessMOVIE.ini
+python3 /opt/mp4_automator/manual.py -i /downloads/complete/movies -a -c /config/autoProcessMOVIE.ini
--- completetv.sh
+++ completetv.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 /opt/mp4_automator/manual.py -i /downloads/complete/tv -c /config/autoProcessTV.ini
+python3 /opt/mp4_automator/manual.py -i /downloads/complete/tv -a -c /config/autoProcessTV.ini
