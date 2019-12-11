4eb3a9fb976ae2fab818e88b0d9af10380289b33
unknown <ianperry99@gmail.com>
2019-12-11 01:22:11 -0500

    update

--- PlexComskip.py
+++ PlexComskip.py
@@ -246,10 +246,10 @@ logging.info('Checking comskipped file')
 try:
     input_size = os.path.getsize(os.path.abspath(video_path))
     output_size = os.path.getsize(os.path.abspath(os.path.join(temp_dir, video_basename)))
-    if input_size and 1.01 > float(output_size) / float(input_size) > 0.99:
-        logging.warning('Output file size is too similar, not replacing the original: %s -> %s' % (sizeof_fmt(input_size), sizeof_fmt(output_size)))
-        cleanup_and_exit(temp_dir, SAVE_ALWAYS, CONVERSION_DID_NOT_MODIFY_ORIGINAL)
-    elif input_size and 1.1 > float(output_size) / float(input_size) > 0.4:
+    # if input_size and 1.01 > float(output_size) / float(input_size) > 0.99:
+    #    logging.success('Output file size is too similar, not replacing the original: %s -> %s' % (sizeof_fmt(input_size), sizeof_fmt(output_size)))
+    #    cleanup_and_exit(temp_dir, SAVE_ALWAYS, CONVERSION_DID_NOT_MODIFY_ORIGINAL)
+    if input_size and 1.1 > float(output_size) / float(input_size) > 0.4:
         logging.success('Output file size is good, replacing the original: %s -> %s' % (sizeof_fmt(input_size), sizeof_fmt(output_size)))
         logging.info('Copying the output: %s -> %s' % (video_basename, output_video_dir))
         shutil.copy(os.path.join(temp_dir, video_basename), output_video_dir)
